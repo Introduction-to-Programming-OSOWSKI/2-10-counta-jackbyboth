52cd0ed80b80cb0cccee88f7269da483fb5ade29
jackbyboth23 <817581@mystma.org>
2021-10-06 14:53:45 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #WRITE YOUR CODE IN THIS FILE
+def countA(w):
+    
+    total = 0 
+    
+    for i in range(0, len(w)):
+        if w[i] == "a":
+            total = total + 1
+    return total
+print (countA("cat"))
